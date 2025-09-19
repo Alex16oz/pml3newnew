16317708cdeef8cd707f27701e0d064f7f2cef4e
Alex16oz <85593164+Alex16oz@users.noreply.github.com>
2025-09-19 10:45:46 +0700

    v6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -62,3 +62,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
+apply(plugin = "com.google.gms.google-services")
